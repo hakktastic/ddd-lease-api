768f4683faabce2f1ba15caff5785fa980783fef
Cabuk, Hakki (AV) <HCabuk@svb.nl>
2024-06-20 15:31:10 +0200

    update test with verification of modulith modules & writing of documentation
    - to verify the modules
    - create documentation

--- src/test/java/nl/svb/dms/ddd_lease_api/DddLeaseApiApplicationTests.java
+++ src/test/java/nl/svb/dms/ddd_lease_api/DddLeaseApiApplicationTests.java
@@ -1,13 +1,44 @@
 package nl.svb.dms.ddd_lease_api;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
 @SpringBootTest
 class DddLeaseApiApplicationTests {
 
-    @Test
-    void contextLoads() {
+    private ApplicationModules modules;
+
+    @BeforeEach
+    void beforeEach(){
+        modules = ApplicationModules.of(DddLeaseApiApplication.class);
     }
 
+    @Test
+    void verifyModules() {
+
+        modules.forEach(applicationModule -> log.info("Module: '{}', package: '{}'", applicationModule.getName(),
+            applicationModule.getBasePackage()));
+
+        modules.verify();
+
+
+    }
+
+    @Test
+    void writeDocumentation(){
+
+        final var documenter = new Documenter(modules).writeDocumentation()
+            .writeModuleCanvases()
+            .writeIndividualModulesAsPlantUml()
+            .writeModulesAsPlantUml();
+
+        assertThat(documenter).isNotNull();
+    }
 }
