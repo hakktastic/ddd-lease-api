8c012da7d11d37ffd8d3a85113d56fbc69c081fb
hakktastic <hakktastic@gmail.com>
2024-06-25 16:44:43 +0200

    add bean configuration for service
    to make the domain service available to the spring context

---
+++ src/main/java/nl/svb/dms/ddd_lease_api/sales/infrastructure/BeanConfiguration.java
@@ -0,0 +1,16 @@
+package nl.svb.dms.ddd_lease_api.sales.infrastructure;
+
+import nl.svb.dms.ddd_lease_api.sales.domain.DomainQuoteService;
+import nl.svb.dms.ddd_lease_api.sales.domain.QuoteRepository;
+import nl.svb.dms.ddd_lease_api.sales.domain.QuoteService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfiguration {
+
+    @Bean
+    QuoteService quoteService(final QuoteRepository quoteRepository) {
+        return new DomainQuoteService(quoteRepository);
+    }
+}
