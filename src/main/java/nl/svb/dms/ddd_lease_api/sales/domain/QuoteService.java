b1fd908ab6da95c147630a36e77cda08d07b7019
hakktastic <hakktastic@gmail.com>
2024-07-31 22:10:16 +0200

    update fill out of quote (WIP)
    - create aggregate
    - start with handling the command for filling out quotes

--- src/main/java/nl/svb/dms/ddd_lease_api/sales/domain/QuoteService.java
+++ src/main/java/nl/svb/dms/ddd_lease_api/sales/domain/QuoteService.java
@@ -1,12 +1,46 @@
 package nl.svb.dms.ddd_lease_api.sales.domain;
 
+import lombok.RequiredArgsConstructor;
+import nl.svb.dms.ddd_lease_api.sales.domain.aggregate.Quote;
+import nl.svb.dms.ddd_lease_api.sales.domain.aggregate.QuoteEntity;
+import nl.svb.dms.ddd_lease_api.sales.domain.aggregate.car.CarBrand;
+import nl.svb.dms.ddd_lease_api.sales.domain.aggregate.car.CarCatalogPrice;
+import nl.svb.dms.ddd_lease_api.sales.domain.aggregate.car.CarModel;
+import nl.svb.dms.ddd_lease_api.sales.domain.aggregate.customer.*;
+import nl.svb.dms.ddd_lease_api.sales.domain.aggregate.lease.LeaseDuration;
+import nl.svb.dms.ddd_lease_api.sales.domain.aggregate.lease.LeaseMileage;
+import nl.svb.dms.ddd_lease_api.sales.domain.aggregate.quote.FillOutQuoteResult;
+import nl.svb.dms.ddd_lease_api.sales.domain.command.FillOutQuote;
+
 import java.util.Optional;
 
-public interface QuoteService {
+@RequiredArgsConstructor
+public final class QuoteService {
 
-    Optional<Quote> fillOut(Quote quote);
+    private final QuoteRepository quoteRepository;
 
-    void calculateInstallment();
+    public Optional<FillOutQuoteResult> fillOut(LeaseDuration duration, LeaseMileage mileage, CustomerFirstName customerFirstName,
+                                                CustomerLastName customerLastName, CustomerEmail customerEmail,
+                                                CustomerBirthDate customerBirthDate, CustomerYearlyIncome customerYearlyIncome,
+                                                CarBrand brandName, CarModel model, CarCatalogPrice carCatalogPrice) {
 
-    void sign();
+
+        final var quote = Quote.of(QuoteEntity.of(duration, mileage, customerFirstName, customerLastName, customerEmail,
+                customerBirthDate, customerYearlyIncome, brandName, model, carCatalogPrice));
+        
+        final var fillOutQuoteResult = quote.handleCommand(FillOutQuote.of(duration, mileage, customerFirstName,
+                customerLastName, customerEmail, customerBirthDate, customerYearlyIncome, brandName, model, carCatalogPrice));
+
+        // handle result
+
+        return null;
+    }
+
+    public void calculateInstallment() {
+
+    }
+
+    public void sign() {
+
+    }
 }
