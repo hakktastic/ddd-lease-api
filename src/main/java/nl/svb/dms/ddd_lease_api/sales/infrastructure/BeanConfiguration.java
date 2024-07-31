1ba23d213f70046b3e037b27a265da756ce66483
hakktastic <hakktastic@gmail.com>
2024-07-31 22:06:38 +0200

    add todo to bean configuration
    perhaps it's not needed since I'm not using a domain interface anymore

--- src/main/java/nl/svb/dms/ddd_lease_api/sales/infrastructure/BeanConfiguration.java
+++ src/main/java/nl/svb/dms/ddd_lease_api/sales/infrastructure/BeanConfiguration.java
@@ -1,16 +1,17 @@
 package nl.svb.dms.ddd_lease_api.sales.infrastructure;
 
-import nl.svb.dms.ddd_lease_api.sales.domain.DomainQuoteService;
 import nl.svb.dms.ddd_lease_api.sales.domain.QuoteRepository;
 import nl.svb.dms.ddd_lease_api.sales.domain.QuoteService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+// TODO check if this class is needed, since I don't have a DomainService interface anymore
 @Configuration
 class BeanConfiguration {
 
     @Bean
     QuoteService quoteService(final QuoteRepository quoteRepository) {
-        return new DomainQuoteService(quoteRepository);
+
+        return new QuoteService(quoteRepository);
     }
 }
