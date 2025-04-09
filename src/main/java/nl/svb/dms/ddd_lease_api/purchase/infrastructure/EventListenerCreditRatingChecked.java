108b98d525d54c70ac604d6e8c6a7b5667a4f4a7
hakktastic <hakktastic@gmail.com>
2025-04-09 20:44:15 +0200

    add bounded contact purchase and receive credit rating checked event
    - final step regarding this implementation

---
+++ src/main/java/nl/svb/dms/ddd_lease_api/purchase/infrastructure/EventListenerCreditRatingChecked.java
@@ -0,0 +1,18 @@
+package nl.svb.dms.ddd_lease_api.purchase.infrastructure;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import nl.svb.dms.ddd_lease_api.legal.domain.event.CreditRatingCheckedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class EventListenerCreditRatingChecked {
+
+    @EventListener
+    void on(CreditRatingCheckedEvent creditRatingCheckedEvent) {
+        log.info("Credit rating checked event received: {}", creditRatingCheckedEvent);
+    }
+}
---
+++ src/main/java/nl/svb/dms/ddd_lease_api/purchase/package-info.java
@@ -0,0 +1,4 @@
+@BoundedContext(name = "purchase", description = "Bounded Context for the purchase domain model of the lease API")
+package nl.svb.dms.ddd_lease_api.purchase;
+
+import org.jmolecules.ddd.annotation.BoundedContext;
